838638b74dc8e6ec5615eb20303ee87afb044a55
edriso <edriso.dev@gmail.com>
2023-10-08 14:26:45 +0300

    add folder structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
