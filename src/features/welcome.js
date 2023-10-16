6f722d2ae6994bf006485dc5b9c91dd89c73f83d
edriso <edriso.dev@gmail.com>
2023-10-17 00:05:42 +0300

    modify the message on welcome

--- src/features/welcome.js
+++ src/features/welcome.js
@@ -1,16 +1,20 @@
 export const initialize = (client) => {
   client.on('guildMemberAdd', (member) => {
-    const introductionChannel = member.guild.channels.cache.find(
-      (channel) => channel.name === 'introduction',
-    );
+    let introductionChannel;
+    let generalChannel;
+    member.guild.channels.cache.filter((channel) => {
+      if (channel.name === 'introduction') introductionChannel = channel;
+      if (channel.name === 'general') generalChannel = channel;
+    });
 
     if (introductionChannel) {
       introductionChannel.send(
-        `Welcome to the server, ${member}! Please introduce yourself here.`,
-      );
-
-      member.send(
-        'Welcome to the server! Please check out the #rules channel and #general channel for more information.',
+        `Hello, ${member}! Welcome to the Pitch Labs Community Server.
+We're here to cultivate a space where aspiring entrepreneurs can learn the art of crafting new businesses. We're genuinely thrilled to have you here!
+Please take a moment to introduce yourself here in the ${introductionChannel} channel. Additionally, kindly share with us in the ${
+          generalChannel || 'general'
+        } channel why you joined so that we can better assist you on your entrepreneurial journey.
+Thank you!`,
       );
     } else {
       console.log('Introduction channel not found.');
