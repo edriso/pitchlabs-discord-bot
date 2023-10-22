54e6d9012ff5250abc3f0abdfd2f1987a9e54191
edriso <edriso.dev@gmail.com>
2023-10-22 13:22:20 +0300

    add channel types and names constants

--- src/features/welcome.js
+++ src/features/welcome.js
@@ -1,21 +1,21 @@
 import client from '../bot.js';
+import { ChannelNames } from '../utils/constants.js';
 
 export const initialize = () => {
   client.on('guildMemberAdd', (member) => {
     let introductionChannel;
     let generalChannel;
     member.guild.channels.cache.filter((channel) => {
-      if (channel.name === 'introduction') introductionChannel = channel;
-      if (channel.name === 'general') generalChannel = channel;
+      if (channel.name === ChannelNames.INTRODUCTION)
+        introductionChannel = channel;
+      if (channel.name === ChannelNames.GENERAL) generalChannel = channel;
     });
 
     if (introductionChannel) {
       introductionChannel.send(
         `Hello, ${member}! Welcome to the Pitch Labs Community Server.
 We're here to cultivate a space where aspiring entrepreneurs can learn the art of crafting new businesses. We're genuinely thrilled to have you here!
-Please take a moment to introduce yourself here in the ${introductionChannel} channel. Additionally, kindly share with us in the ${
-          generalChannel || 'general'
-        } channel why you joined so that we can better assist you on your entrepreneurial journey.
+Please take a moment to introduce yourself here in the ${introductionChannel} channel. Additionally, kindly share with us in the ${generalChannel} channel why you joined so that we can better assist you on your entrepreneurial journey.
 Thank you!`,
       );
     } else {
