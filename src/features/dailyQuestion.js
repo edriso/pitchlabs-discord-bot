3786236631ef1747eeae53a9d8a9bc4b437f8870
edriso <edriso.dev@gmail.com>
2023-10-22 13:49:25 +0300

    add daily question feature

---
+++ src/features/dailyQuestion.js
@@ -0,0 +1,29 @@
+import cron from 'node-cron';
+import client from '../bot.js';
+import { ChannelTypes, ChannelNames } from '../utils/constants.js';
+import { questions } from '../utils/questions.js';
+
+export const initialize = () => {
+  // console.log(`Server Time: ${new Date().toString()}`); // Log the current server time
+  // This will run the task every day at 9:00 AM
+  cron.schedule('0 9 * * *', () => {
+    //   cron.schedule('*/3 * * * * *', () => { // each 3 seconds
+    client.guilds.cache.forEach((guild) => {
+      const generalChannel = guild.channels.cache.find(
+        (channel) =>
+          channel.type === ChannelTypes.GUILD_TEXT &&
+          channel.name === ChannelNames.GENERAL,
+      );
+
+      if (generalChannel) {
+        const randomQuestion =
+          questions[Math.floor(Math.random() * questions.length)];
+        generalChannel.send(`**Question of the Day:**\n${randomQuestion}`);
+      } else {
+        console.error(
+          `${ChannelNames.GENERAL} channel not found in ${guild.name}.`,
+        );
+      }
+    });
+  });
+};
