c28062347b6e50c68d465842c2841bbc85448faf
edriso <edriso.dev@gmail.com>
2023-10-24 02:52:48 +0300

    add mute reason as a parameter

--- src/utils/muteMember.js
+++ src/utils/muteMember.js
@@ -1,8 +1,8 @@
 import { MUTE_DURATION } from '../config/constants.js';
 
-const muteMember = async (member) => {
+const muteMember = async (member, reason) => {
   try {
-    await member.timeout(MUTE_DURATION, 'Muted for spamming');
+    await member.timeout(MUTE_DURATION, reason);
   } catch (error) {
     console.error(`Error while trying to mute ${member.user.username}`, error);
   }
