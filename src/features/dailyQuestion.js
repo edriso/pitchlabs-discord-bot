10e3b603484d5692216b974b4cb98106237e047a
edriso <edriso.dev@gmail.com>
2023-10-23 23:23:09 +0300

    bugfix: dailyQuestion will not repeat two days in a row

--- src/features/dailyQuestion.js
+++ src/features/dailyQuestion.js
@@ -3,11 +3,16 @@ import client from '../bot.js';
 import { ChannelTypes, ChannelNames } from '../utils/constants.js';
 import { questions } from '../utils/questions.js';
 
+let lastQuestionAsked = '';
+const getRandomQuestion = () => {
+  return questions[Math.floor(Math.random() * questions.length)];
+};
+
 export const initialize = () => {
   // console.log(`Server Time: ${new Date().toString()}`); // Log the current server time
   // This will run the task every day at 9:00 AM
   cron.schedule('0 9 * * *', () => {
-    //   cron.schedule('*/3 * * * * *', () => { // each 3 seconds
+    // cron.schedule('*/3 * * * * *', () => {// each 3 seconds
     client.guilds.cache.forEach((guild) => {
       const generalChannel = guild.channels.cache.find(
         (channel) =>
@@ -16,8 +21,13 @@ export const initialize = () => {
       );
 
       if (generalChannel) {
-        const randomQuestion =
-          questions[Math.floor(Math.random() * questions.length)];
+        let randomQuestion = getRandomQuestion();
+
+        while (randomQuestion === lastQuestionAsked) {
+          randomQuestion = getRandomQuestion();
+        }
+        lastQuestionAsked = randomQuestion;
+
         generalChannel.send(`**Question of the Day:**\n${randomQuestion}`);
       } else {
         console.error(
