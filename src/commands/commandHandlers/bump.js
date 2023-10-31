fdcff401817711e23bf77b853ae8d1bb2e471d98
edriso <edriso.dev@gmail.com>
2023-10-31 03:29:35 +0200

    save lastBumpRecord on db

--- src/commands/commandHandlers/bump.js
+++ src/commands/commandHandlers/bump.js
@@ -1,20 +1,34 @@
 import { BUMP_COOLDOWN_DURATION } from '../../config/constants.js';
-
-let lastBumpTimestamp = null;
+import Bump from '../../models/bumpModel.js';
 
 const bump = async (interaction) => {
-  const currentTimestamp = Date.now();
-  if (
-    !lastBumpTimestamp ||
-    currentTimestamp - lastBumpTimestamp >= BUMP_COOLDOWN_DURATION
-  ) {
-    lastBumpTimestamp = currentTimestamp;
-    interaction.reply('Server successfully bumped!');
-  } else {
+  try {
+    const currentTimestamp = Date.now();
+    const lastBumpRecord = await Bump.findOne();
+
+    if (
+      !lastBumpRecord ||
+      currentTimestamp - lastBumpRecord.bumpedAt >= BUMP_COOLDOWN_DURATION
+    ) {
+      // update or create a new record with the current timestamp
+      await Bump.findOneAndUpdate(
+        {},
+        { bumpedAt: currentTimestamp },
+        { upsert: true },
+      );
+      interaction.reply('Server successfully bumped!');
+    } else {
+      interaction.reply({
+        content: `Sorry, the server was already bumped. Please wait ${
+          BUMP_COOLDOWN_DURATION / (1000 * 60)
+        } minutes from the last bump.`,
+        ephemeral: true,
+      });
+    }
+  } catch (error) {
+    console.error('Error handling bump command:', error);
     interaction.reply({
-      content: `Sorry, the server was already bumped. Please wait ${
-        BUMP_COOLDOWN_DURATION / (1000 * 60)
-      } minutes from the last bump.`,
+      content: 'An error occurred while processing your request.',
       ephemeral: true,
     });
   }
